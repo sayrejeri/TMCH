f9af844fba22875bbf6a6335451dadc8188eec5e
sayrejeri <152928057+sayrejeri@users.noreply.github.com>
2025-12-25 04:30:35 -0500

    Add macros plugin and global variables

---
+++ main.py
@@ -0,0 +1,5 @@
+def define_env(env):
+    env.variables["discord_invite"] = "https://discord.gg/MWgeYF7Ymp"
+    env.variables["site_url"] = "https://sayrejeri.github.io/TMCH/"
+    env.variables["repo_url"] = "https://github.com/sayrejeri/TMCH"
+    env.variables["official_contact"] = "http://www.totalminer.com/contact.html"
